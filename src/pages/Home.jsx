f17dd5151f6c1d3c191af49108d6dde96778b720
bonny-art <svitlana.otenko@gmail.com>
2023-12-24 15:27:12 +0200

    makes Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,36 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [error, setError] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      setError('');
+
+      try {
+        const resp = await getCountries();
+        setCountries(resp);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {isLoading && <Loader />}
+        {error && <Heading>{error}</Heading>}
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
